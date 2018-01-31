099b91e3ee63947ba1b98f695e6bd0d05b2a0334
RIscRIpt <richard@riscript.com>
2018-01-31 16:56:36 +0200

    initial commit

---
+++ librlcom.go
@@ -0,0 +1,61 @@
+package librlcom
+
+import (
+	"encoding/binary"
+	"errors"
+	"net"
+	"reflect"
+)
+
+var (
+	ErrUnknownMessage = errors.New("unknown message")
+)
+
+type Courier struct {
+	conn net.Conn
+}
+
+func NewCourier(conn net.Conn) *Courier {
+	return &Courier{
+		conn: conn,
+	}
+}
+
+func (c *Courier) readHeader() (header Header, err error) {
+	err = binary.Read(c.conn, binary.LittleEndian, &header)
+	return
+}
+
+func (c *Courier) Receive() (Message, error) {
+	var header Header
+	_, err := header.ReadFrom(c.conn)
+	if err != nil {
+		return nil, err
+	}
+	if t, ok := typeMapMsg2Go[header.MessageType]; ok {
+		msg := reflect.New(t).Elem().Interface().(Message)
+		_, err = msg.ReadFrom(c.conn)
+		if err != nil {
+			return &header, err
+		}
+		return msg, nil
+	}
+	return &header, ErrUnknownMessage
+}
+
+func (c *Courier) Send(msg Message) error {
+	if t, ok := typeMapGo2Msg[reflect.TypeOf(msg)]; ok {
+		var header Header
+		header.MessageSize = uint64(msg.Size())
+		header.MessageType = t
+		_, err := header.WriteTo(c.conn)
+		if err != nil {
+			return err
+		}
+		_, err = msg.WriteTo(c.conn)
+		if err != nil {
+			return err
+		}
+	}
+	return ErrUnknownMessage
+}
---
+++ messages.go
@@ -0,0 +1,173 @@
+package librlcom
+
+import (
+	"encoding/binary"
+	"io"
+	"reflect"
+)
+
+type Type uint64
+
+// Message types
+const (
+	MTOK Type = 0x0C << 56
+
+	MTVersion       Type = 0x01
+	MTAuthorization Type = 0x0A
+	MTLinkLibrary   Type = 0x111B
+
+	MTGetSymbolLibrary      Type = 0x63751711B
+	MTResolvedSymbolLibrary Type = 0x435013D417
+)
+
+type Header struct {
+	MessageSize uint64
+	MessageType Type
+}
+
+type Message interface {
+	io.ReaderFrom
+	io.WriterTo
+	Size() int64
+}
+
+type Token [128]byte
+type String string
+
+type OK struct{}
+
+type Version struct{ Value uint64 }
+type Authorization struct{ Token }
+type LinkLibrary struct{ String }
+
+type GetSymbolLibrary struct{ String }
+type ResolvedSymbolLibrary struct{ String }
+
+var typeMapMsg2Go = map[Type]reflect.Type{
+	MTVersion:       reflect.TypeOf(Version{}),
+	MTAuthorization: reflect.TypeOf(Authorization{}),
+	MTLinkLibrary:   reflect.TypeOf(LinkLibrary{}),
+
+	MTOK: reflect.TypeOf(OK{}),
+}
+
+var typeMapGo2Msg = map[reflect.Type]Type{
+	reflect.TypeOf(Version{}):       MTVersion,
+	reflect.TypeOf(Authorization{}): MTAuthorization,
+	reflect.TypeOf(LinkLibrary{}):   MTLinkLibrary,
+
+	reflect.TypeOf(OK{}): MTOK,
+}
+
+func (h *Header) ReadFrom(r io.Reader) (n int64, err error) {
+	err = binary.Read(r, binary.LittleEndian, h)
+	if err != nil {
+		return 0, err
+	}
+	return h.Size(), nil
+}
+
+func (h *Header) WriteTo(w io.Writer) (n int64, err error) {
+	err = binary.Write(w, binary.LittleEndian, h)
+	if err != nil {
+		return 0, err
+	}
+	return h.Size(), nil
+}
+
+func (h *Header) Size() int64 {
+	return int64(binary.Size(*h))
+}
+
+func (ok *OK) ReadFrom(r io.Reader) (n int64, err error) {
+	return 0, nil
+}
+
+func (ok *OK) WriteTo(w io.Writer) (n int64, err error) {
+	return 0, nil
+}
+
+func (ok *OK) Size() int64 {
+	return 0
+}
+
+func (t *Token) ReadFrom(r io.Reader) (n int64, err error) {
+	err = binary.Read(r, binary.LittleEndian, *t)
+	if err != nil {
+		return 0, err
+	}
+	return t.Size(), nil
+}
+
+func (t *Token) WriteTo(w io.Writer) (n int64, err error) {
+	err = binary.Write(w, binary.LittleEndian, (*[128]byte)(t))
+	if err != nil {
+		return 0, err
+	}
+	return t.Size(), nil
+}
+
+func (t *Token) Size() int64 {
+	return int64(binary.Size(*t))
+}
+
+func (s *String) ReadFrom(r io.Reader) (n int64, err error) {
+	var length uint64
+	err = binary.Read(r, binary.LittleEndian, &length)
+	if err != nil {
+		return 0, err
+	}
+	var m int
+	str := make([]byte, length)
+	for k := uint64(0); k < length; k += uint64(m) {
+		m, err = r.Read(str[k:])
+		if err != nil {
+			*s = String(str)
+			return int64(k), err
+		}
+	}
+	*s = String(str)
+	return int64(length), nil
+}
+
+func (s *String) WriteTo(w io.Writer) (n int64, err error) {
+	var length uint64 = uint64(len(*s)) + 1
+	err = binary.Write(w, binary.LittleEndian, &length)
+	if err != nil {
+		return 0, err
+	}
+	m, err := io.WriteString(w, string(*s))
+	if err != nil {
+		return 8 + int64(m), err
+	}
+	null := [...]byte{0}
+	m, err = w.Write(null[:])
+	if err != nil {
+		return 8 + int64(len(*s)) + int64(m), err
+	}
+	return 8 + int64(len(*s)) + 1, nil
+}
+
+func (s *String) Size() int64 {
+	return int64(len(*s)) + 1
+}
+
+func (v *Version) ReadFrom(r io.Reader) (n int64, err error) {
+	err = binary.Read(r, binary.LittleEndian, &v.Value)
+	if err != nil {
+		return 0, err
+	}
+	return v.Size(), nil
+}
+
+func (v *Version) WriteTo(w io.Writer) (n int64, err error) {
+	err = binary.Write(w, binary.LittleEndian, &v.Value)
+	if err != nil {
+		return 0, err
+	}
+	return v.Size(), nil
+}
+
+func (v *Version) Size() int64 {
+	return int64(binary.Size(*v))
+}
